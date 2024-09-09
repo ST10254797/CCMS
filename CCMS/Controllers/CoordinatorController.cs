8377c44734ff23ecdb3ca83e28ebf37a4a6dacad
ST10254797 <128587735+ST10254797@users.noreply.github.com>
2024-09-09 14:54:51 +0200

    Update CoordinatorController.cs

--- CCMS/Controllers/CoordinatorController.cs
+++ CCMS/Controllers/CoordinatorController.cs
@@ -11,3 +11,5 @@ namespace CCMS.Controllers
         }
     }
 }
+  //References:
+    //Bootstrap, ND. Build fast, responsive sites with Bootstrap. [Online]. Available at: https://getbootstrap.com/ (Accessed 5 September 2024)
